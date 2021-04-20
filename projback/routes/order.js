b047033ccbd97cb2ad90ca7fa2647a40881266e9
Vinesh Kumar <63194027+vineshk711@users.noreply.github.com>
2021-04-20 15:17:11 +0530

    update

---
+++ projback/routes/order.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
+const { updateStock } = require("../controllers/product");
+
+const {} = require("../controllers/order");
