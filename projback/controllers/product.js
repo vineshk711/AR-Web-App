7e2cc2360a780f10bd459cbcf958b178e1a95d08
Vinesh Kumar <63194027+vineshk711@users.noreply.github.com>
2021-04-20 15:00:06 +0530

    update

--- projback/controllers/product.js
+++ projback/controllers/product.js
@@ -150,3 +150,24 @@ exports.getAllProducts = (req, res) => {
       res.json(products);
     });
 };
+
+// automatically manage stock and sold handled in frontend
+exports.updateStock = (req, res, next) => {
+  let myOperations = req.body.order.products.map((prod) => {
+    return {
+      updateOne: {
+        filter: { _id: prod._id },
+        update: { $inc: { stock: -prod.count, sold: +prod.count } }
+      }
+    };
+  });
+
+  Product.bulkWrite(myOperations, {}, (err, products) => {
+    if (err) {
+      res.status(400).json({
+        error: "Unable execute BULK operation in stock"
+      });
+    }
+    next();
+  });
+};
