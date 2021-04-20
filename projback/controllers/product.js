87f45246d248646baaa651780121d1c1c0962b0a
Vinesh Kumar <63194027+vineshk711@users.noreply.github.com>
2021-04-20 13:57:16 +0530

    update

--- projback/controllers/product.js
+++ projback/controllers/product.js
@@ -2,6 +2,7 @@ const Product = require("../models/product");
 const formidable = require("formidable");
 const _ = require("lodash");
 const fs = require("fs");
+const { runInNewContext } = require("vm");
 
 exports.getProductById = (req, res, next, id) => {
   Product.findById(id)
@@ -55,6 +56,74 @@ exports.createProduct = (req, res) => {
       if (err) {
         res.status(400).json({
           error: "Saving tshirt in DB failed"
+        });
+      }
+      res.json(product);
+    });
+  });
+};
+
+exports.getProduct = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
+
+// middleware
+exports.photo = (req, res, next) => {
+  if (req.product.photo.data) {
+    res.set("Content-type", req.product.photo.contentType);
+    return res.send(req.product.photo.data);
+  }
+  next();
+};
+
+exports.deleteProduct = (req, res) => {
+  let product = req.product;
+  product.remove((err, removedProduct) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Failed to delete product",
+        product: product.name
+      });
+    }
+    res.json({
+      message: "Product deleted successully",
+      deletedProduct: removedProduct.name
+    });
+  });
+};
+
+exports.updateProduct = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "unable to update"
+      });
+    }
+    // updation
+    let product = req.product;
+    product = _.extend(product, fields);
+
+    //handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "File size too big!"
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+    // console.log(product);
+
+    //save to the DB
+    product.save((err, product) => {
+      if (err) {
+        res.status(400).json({
+          error: "Updation failed"
         });
       }
       res.json(product);
--- projback/routes/product.js
+++ projback/routes/product.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProductById, createProduct } = require("../controllers/product");
+const {
+  getProductById,
+  createProduct,
+  getProduct,
+  photo,
+  updateProduct,
+  deleteProduct
+} = require("../controllers/product");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
@@ -10,6 +17,7 @@ router.param("userId", getUserById);
 router.param("productId", getProductById);
 
 //all of actual routes
+// create
 router.post(
   "/product/create/:userId",
   isSignedIn,
@@ -17,5 +25,29 @@ router.post(
   isAdmin,
   createProduct
 );
+
+// read
+router.get("/product/:productId", getProduct);
+router.get("/product/photo/:productId", photo);
+
+// update
+router.put(
+  "/product/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateProduct
+);
+
+// delete
+router.delete(
+  "/product/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  deleteProduct
+);
+
+// listing the product according to user need
 
 module.exports = router;
