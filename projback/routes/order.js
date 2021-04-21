b7cdfa49a244cad478de323d4cdf7886c206ab7e
Vinesh Kumar <63194027+vineshk711@users.noreply.github.com>
2021-04-21 17:53:42 +0530

    update

--- projback/routes/order.js
+++ projback/routes/order.js
@@ -5,10 +5,21 @@ const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
 const { updateStock } = require("../controllers/product");
 
-const { getOrderById } = require("../controllers/order");
+const { getOrderById, createOrder } = require("../controllers/order");
 
 // params
 router.param("userId", getUserById);
 router.param("orderId", getOrderById);
+
+// routes
+// create
+router.post(
+  "/order/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  pushOrderInPurchaseList,
+  updateStock,
+  createOrder
+);
 
 module.exports = router;
