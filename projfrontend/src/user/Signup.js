2c4d1faa31b6564cbe1ea5b70af33315add62a9d
Vinesh Kumar <Vineshk711@gmail.com>
2021-05-10 13:52:29 +0000

    update

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -4,9 +4,32 @@ import Base from '../core/Base'
 import {Link} from 'react-router-dom'
 
 const Signup = () => {
+    const signUpForm = () => {
+        return (
+            <div className="row">
+                <div className="col-mid-6 offset-sm-3 text-left">
+                    <form>
+                        <div className="form-group">
+                            <label className="text-light">Name</label>
+                            <input className="form-control" type="text" />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input className="form-control" type="email" />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Password</label>
+                            <input className="form-control" type="password" />
+                        </div>
+                        <button className="btn btn-success btn-block">Submit</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
     return(
         <Base title="Sing up Page" description="Apge for user to sign up!">
-            <h1>Sign up works</h1>
+            {signUpForm()}
         </Base>
     )
 }
